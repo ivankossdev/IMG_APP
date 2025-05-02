139c756119b22cdeae6385ce63a9bd6eb4b24ffc
ivan.koss <ivan.koss@yandex.ru>
2025-05-03 00:18:20 +0300

    create test func pbm file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Text;
+using System.IO;
 
 namespace img_app;
 
@@ -7,8 +8,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        // test.bmp
-        GetInfo test = new GetInfo("test.bmp");
-        test.Info();
+        createFile("test.pbm");
+    }
+
+    static void createFile(string fileName){
+       string path = $"{Directory.GetCurrentDirectory()}/{fileName}";
+        try
+        {
+            using (FileStream fs = File.Create(path))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes("P1\n4 4\n1111\n1001\n1001\n1111\n");
+                fs.Write(info, 0, info.Length);
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
     }
 }
