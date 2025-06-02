fa88c6ba60e22e915b8a14fb4ceaf2a9cf9380aa
ivankossdev <ivan.koss@yandex.ru>
2025-06-02 14:05:46 +0300

    Create random method

--- Program.cs
+++ Program.cs
@@ -9,12 +9,22 @@ class Program
     static void Main(string[] args)
     {
         const int w = 82, h = 64;
-        Image_BMP image_BMP = new();
-        image_BMP.Name = $"image_{w}x{h}";
+        Image_BMP image_BMP = new()
+        {
+            Name = $"image_{w}x{h}"
+        };
+        
         image_BMP.CreateBMP(w, h);
 
-        GetFileInfo getFileInfo = new();
-        getFileInfo.Name = $"example/image_{w}x{h}.bmp";
-        getFileInfo.GetAllByte();
+        Editor editor = new()
+        {
+            Name = image_BMP.Name
+        };
+
+        editor.CreateRandom();
+
+        // GetFileInfo getFileInfo = new();
+        // getFileInfo.Name = $"example/image_{w}x{h}.bmp";
+        // getFileInfo.GetAllByte();
     }
 }
--- image/editor.cs
+++ image/editor.cs
@@ -2,5 +2,24 @@ namespace img_app;
 
 public class Editor : File
 {
-    
+    protected string GetFileType()
+    {
+        string[] str = Name.Split('.');
+        return str.Length > 0 ? str[^1] : $"File not found type";
+    }
+
+    public void CreateRandom()
+    {
+        string fileType = GetFileType();
+        switch (fileType)
+        {
+            case "bmp":
+                System.Console.WriteLine("Detectep bmp file");
+                break;
+
+            default:
+                System.Console.WriteLine(fileType);
+                break;
+        }
+    }
 }
