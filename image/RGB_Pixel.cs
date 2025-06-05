24cd1cf044d8e71fff6f3ed2490e3d899dde04d7
ivankossdev <ivan.koss@yandex.ru>
2025-06-05 13:54:30 +0300

    PixelByte method

--- image/RGB_Pixel.cs
+++ image/RGB_Pixel.cs
@@ -8,11 +8,16 @@ class RGB_Pixel : Random
         if (stop > 256) stop = 256;
         if (start < 0) start = 0;
         if (start >= 255) start = 254;
-        
+
         return Next(start, stop);
     }
 
-    public string PixelString(int red = 50, int green = 50, int blue = 50){
+    public string PixelString(int red = 50, int green = 50, int blue = 50)
+    {
         return $"{Generate(red)} {Generate(green)} {Generate(blue)} ";
+    }
+    
+    public byte[] PixelByte(int red = 50, int green = 50, int blue = 50){
+        return [(byte)Generate(blue), (byte)Generate(green), (byte)Generate(red)];
     }
 }
--- image/editor.cs
+++ image/editor.cs
@@ -23,9 +23,12 @@ public class Editor : File
                 Random random = new();
 
                 Image.InsertWord(ref data, infoBMP.LenghtWord, 0, infoBMP.AppBytes, [ 0x00, 0x00, 0xff ]);
+                RGB_Pixel rgb_Pixel = new();
 
                 for (int i = 0; i < 10; i++)
-                    Image.InsertWord(ref data, infoBMP.LenghtWord, (uint)random.Next(0, (int)h - 2), infoBMP.AppBytes, [ 0xff, 0x00, 0x00 ]);
+                {
+                    Image.InsertWord(ref data, infoBMP.LenghtWord, (uint)random.Next(0, (int)h - 2), infoBMP.AppBytes, rgb_Pixel.PixelByte());
+                }
 
                 Image.InsertWord(ref data, infoBMP.LenghtWord, h - 1, infoBMP.AppBytes, [ 0x00, 0xff, 0x00 ]);
                 
