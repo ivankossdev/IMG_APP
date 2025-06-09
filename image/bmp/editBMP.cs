6133dffefaaa3a11a12926b6066284b11f43ea54
ivankossdev <ivan.koss@yandex.ru>
2025-06-09 11:35:33 +0300

    ref editBMP.cs

--- image/bmp/editBMP.cs
+++ image/bmp/editBMP.cs
@@ -1,11 +1,35 @@
 namespace img_app;
 
-public class EditBMP (InfoBMP _infoBMP)
+public class EditBMP
 {
-    private InfoBMP infoBMP = _infoBMP;
-
-    public void Inf()
+    private byte[] data;
+    private readonly uint w;
+    private readonly uint h;
+    private InfoBMP infoBMP;
+    public EditBMP(ref byte[] _data)
     {
-        Console.WriteLine($"{infoBMP.Width} {infoBMP.LenghtWord} {infoBMP.AppBytes}");
+        data = _data;
+        w = Image.GetData(ref data, 18, 21);
+        h = Image.GetData(ref data, 22, 25);
+        infoBMP = new(w);
+    }
+
+    public void FillRandLines()
+    {
+        RGB_Pixel rgb_Pixel = new();
+        for (int i = 0; i < h; i++)
+        {
+            Image.InsertWord(ref data, infoBMP.LenghtWord, (uint)i, infoBMP.AppBytes, rgb_Pixel.PixelByte());
+        }
+    }
+
+    public void AddPixels()
+    {
+        Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 0, 0);
+        Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 1, 0);
+        Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 2, 0);
+        Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 0, 1);
+        Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 1, 1);
+        Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 0, 2);
     }
 }
--- image/editor.cs
+++ image/editor.cs
@@ -3,7 +3,6 @@ namespace img_app;
 
 public class Editor : File
 {
-    readonly RGB_Pixel rgb_Pixel = new();
     protected string GetFileType()
     {
         string[] str = Name.Split('.');
@@ -17,26 +16,9 @@ public class Editor : File
         {
             case "bmp":
                 byte[] data = Read(Name);
-                uint w = Image.GetData(ref data, 18, 21);
-                uint h = Image.GetData(ref data, 22, 25);
-
-                InfoBMP infoBMP = new(w);
-                EditBMP editBMP = new(infoBMP);
-
-                for (int i = 0; i < h - 1; i++)
-                {
-                    Image.InsertWord(ref data, infoBMP.LenghtWord, (uint)i, infoBMP.AppBytes, rgb_Pixel.PixelByte());
-                }
-
-                Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 0, 0);
-                Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 1, 0);
-                Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 2, 0);
-                Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 0, 1);
-                Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 1, 1);
-                Image.InsertPixel(ref data, [0xff, 0x00, 0x00], infoBMP.LenghtWord, 0, 2);
-
-                editBMP.Inf();
-                
+                EditBMP editBMP = new(ref data);
+                editBMP.FillRandLines();
+                editBMP.AddPixels();
                 BinWrite(ref data, RenameFile(Name, "Rand"));
                 break;
             default:
