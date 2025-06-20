4aa59dd24ec1a6bcb8d5901a91862f16d859979a
ivan.koss <ivan.koss@yandex.ru>
2025-06-21 00:41:35 +0300

    GetAngle method

--- Program.cs
+++ Program.cs
@@ -8,18 +8,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        const int w = 640, h = 480;
+        ImageBMP image_BMP = new() { Name = $"image_{w}x{h}" };
+        image_BMP.Create(w, h);
 
-        // const int w = 640, h = 480;
-        // ImageBMP image_BMP = new() { Name = $"image_{w}x{h}" };
-        // image_BMP.Create(w, h);
+        Add add = new(image_BMP.Name);
+        add.XLines(w);
+        add.YLines(h);
 
-        // Add add = new(image_BMP.Name);
-        // add.XLines(w);
-        // add.YLines(h);
-
-        // GetFileInfo getFileInfo = new() { Name = $"example/image_{w}x{h}.bmp" };
-        // getFileInfo.GetAllByte();
-        MathEx.Example();
+        GetFileInfo getFileInfo = new() { Name = $"example/image_{w}x{h}.bmp" };
+        getFileInfo.GetAllByte();
+        MathEx.GetAngle(w, h);
     }
 
 }
