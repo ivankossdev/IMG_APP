e0ab83b611ab51ee30b1ced337c3bd9f2df6de1c
ivan.koss <ivan.koss@yandex.ru>
2025-06-21 23:19:35 +0300

    ref GetAngle method

--- MathEx.cs
+++ MathEx.cs
@@ -5,9 +5,9 @@ public class MathEx
     public static void GetAngle(int w, int h)
     {
         double angle = Math.Atan((double)h / (double)w) * 180 / Math.PI;
-        System.Console.WriteLine(angle);
-        System.Console.WriteLine(180 - 90 - angle);
-        
+        angle = Math.Round(angle);
+        Console.WriteLine(angle);
+        Console.WriteLine(180 - 90 - angle);
     }
     // double degrees = 45;
     // double angle = Math.PI * degrees / 180.0;
