ede4ee27a5e483e81568e2e6d89cf8c084a964d6
ivan.koss <ivan.koss@yandex.ru>
2025-06-29 01:54:10 +0300

    png header file

--- image/png/imagePNG.cs
+++ image/png/imagePNG.cs
@@ -17,11 +17,15 @@ public class ImagePNG
     }
     private byte[] HeaderFile(int width, int height)
     {
-        // 0x89 0x50 0x4e 0x47 0x0d 0x0a 0x1a 0x0a заголовок файла 
         byte[] array = new byte[100];
+        
+        // 0x89 0x50 0x4e 0x47 0x0d 0x0a 0x1a 0x0a заголовок файла png  
+        array[0] = 0x89; array[1] = 0x50; array[2] = 0x4e; array[3] = 0x47;
+        array[4] = 0x0d; array[5] = 0x0a; array[6] = 0x1a; array[7] = 0x0a;
+
         return array;
     }
-        private byte[] Body(ref byte[] array)
+    private byte[] Body(ref byte[] array)
     {
         byte[] _array = new byte[100];
 
