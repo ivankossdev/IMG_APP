677cfd76480e39c2d884166dea33dc4ee63be29d
ivan.koss <ivan.koss@yandex.ru>
2025-07-02 23:35:39 +0300

    cr CRC32 method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.IO.Hashing;
+using System.IO.Compression;
 // dotnet add package System.IO.Hashing
 
 namespace img_app;
@@ -25,7 +26,19 @@ class Program
 
         // GetFileInfo getFileInfoPng = new() { Name = $"test_ex/ex_2px.png" };
         // getFileInfoPng.GetAllByte();
-
+        GetCRC32("example/image_640x480.txt");
     }
+    static void GetCRC32(string path)
+    {
+        Crc32 crc32 = new();
 
+        using FileStream fs = System.IO.File.Open(path, FileMode.Open);
+        // Span<byte> b = [0, 0, 0, 0, 0];
+        string hash = string.Empty;
+        crc32.Append(fs);
+        byte[] checkSum = crc32.GetCurrentHash();
+        Array.Reverse(checkSum);
+        hash = BitConverter.ToString(checkSum).Replace("-", "").ToLower();
+        Console.WriteLine(hash);
+    }
 }
