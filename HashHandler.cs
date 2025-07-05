04fdf9b8d0dbfed772b7a29eaba199f241f635f4
ivan.koss <ivan.koss@yandex.ru>
2025-07-05 10:49:29 +0300

    HashHandler class

---
+++ HashHandler.cs
@@ -0,0 +1,17 @@
+using System.IO.Hashing;
+// dotnet add package System.IO.Hashing
+
+namespace img_app;
+
+public static class HashHandler
+{
+    public static string GetCRC32(Crc32 crc32, string path)
+    {
+        using FileStream fs = System.IO.File.Open(path, FileMode.Open);
+        // Span<byte> b = [0, 0, 0, 0, 0];
+        crc32.Append(fs);
+        byte[] checkSum = crc32.GetCurrentHash();
+        Array.Reverse(checkSum);
+        return BitConverter.ToString(checkSum).Replace("-", "").ToLower();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text;
 using System.IO.Hashing;
-using System.IO.Compression;
 // dotnet add package System.IO.Hashing
 
 namespace img_app;
@@ -26,19 +25,9 @@ class Program
 
         // GetFileInfo getFileInfoPng = new() { Name = $"test_ex/ex_2px.png" };
         // getFileInfoPng.GetAllByte();
-        GetCRC32("example/image_640x480.txt");
-    }
-    static void GetCRC32(string path)
-    {
         Crc32 crc32 = new();
-
-        using FileStream fs = System.IO.File.Open(path, FileMode.Open);
-        // Span<byte> b = [0, 0, 0, 0, 0];
-        string hash = string.Empty;
-        crc32.Append(fs);
-        byte[] checkSum = crc32.GetCurrentHash();
-        Array.Reverse(checkSum);
-        hash = BitConverter.ToString(checkSum).Replace("-", "").ToLower();
-        Console.WriteLine(hash);
+        string hash = HashHandler.GetCRC32(crc32, "example/image_640x480.txt");
+        System.Console.WriteLine(hash);
     }
+
 }
